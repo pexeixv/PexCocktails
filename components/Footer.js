8e346ba46709bf605aa787ca745d5dd61c2115df
Gavin Pereira <68476117+pexeixv@users.noreply.github.com>
2022-11-10 19:54:12 +0530

    footer modified

--- components/Footer.js
+++ components/Footer.js
@@ -1,20 +1,24 @@
+import Link from "next/link";
+
 export default function Footer() {
   return (
-    <footer className="bg-neutral-800">
-      <div className="container mx-auto px-5 py-3 font-bold justify-between flex items-center flex-col md:flex-row gap-3">
-        <a
-          href="//gavn.in"
-          target="_blank"
-          rel="noreferrer"
-          className="flex items-center"
-        >
-          <img src="/code.svg" alt="Developed" className="h-6 mr-3" />
-          by Gavin Pereira
-        </a>
-        <a href="//www.thecocktaildb.com" target="_blank" rel="noreferrer">
-          <img src="/TheCocktailDB.png" alt="TheCocktailDB" className="h-8" />
-        </a>
-      </div>
-    </footer>
+    <>
+      <footer className="bg-neutral-800">
+        <div className="container mx-auto px-5 py-3  justify-between flex items-center flex-col md:flex-row gap-3">
+          <a href="#" className=" inline text-3xl font-black  font-bebas">
+            PexCocktails
+          </a>
+
+          <div className="flex items-center gap-8 justify-center">
+            <Link href="/privacy">
+              <a>Privacy Policy</a>
+            </Link>
+            <Link href="/contact">
+              <a>Contact Us</a>
+            </Link>
+          </div>
+        </div>
+      </footer>
+    </>
   );
 }
