4a048e36eca8a35106a8678c5a0eecc5278255bd
Gavin Pereira <68476117+pexeixv@users.noreply.github.com>
2026-03-19 17:53:42 +0530

    Remove email

--- pages/contact.js
+++ pages/contact.js
@@ -10,14 +10,7 @@ const Contact = () => {
           <h2 className=" text-3xl lg:text-4xl font-bold text-center">
             Contact Us
           </h2>
-          <div className="flex items-center justify-center mt-16">
-            <a
-              href="mailto:hi@gavn.in"
-              className="text-2xl mx-auto rounded-full border-8 py-3 px-8 font-bold inline-block hover:bg-white transition-colors duration-300 border-white hover:text-neutral-900"
-            >
-              hi@gavn.in
-            </a>
-          </div>
+          <div className="flex items-center justify-center mt-16"></div>
         </div>
       </section>
     </>
